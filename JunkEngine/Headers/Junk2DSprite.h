930488b06b49a16386ae4c859ac37860bbc25273
kms <kmu9842@naver.com>
2015-03-16 22:28:48 +0900

    set junk2dsprite 1

---
+++ JunkEngine/Headers/Junk2DSprite.h
@@ -0,0 +1,23 @@
+#ifndef _JUNK2DSPRITE_H             
+#define _JUNK2DSPRITE_H           
+#define WIN32_LEAN_AND_MEAN
+
+class 
+{
+public:
+	//MyClass();
+	//~MyClass();
+
+private:
+
+};
+/*
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+*/
+#endif
